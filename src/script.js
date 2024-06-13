606d834134ccaeaf14410938b063ef496a02a422
Alves Hatewa <alvesmazy90@gmail.com>
2024-06-13 13:35:06 +0100

    Primeira publicacao

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// Abrir e fechar menu
+const menuMobile = document.querySelector("#mobile-menu");
+const btnMenu = document.querySelector("#menu-button");
+
+btnMenu.addEventListener("click", () => {
+    menuMobile.classList.toggle("hidden");
+});
+
+
+//Muda a cor do header
